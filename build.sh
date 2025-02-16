4d36df78261fc677cd40d6db5ed8f025d054aef8
OaryanO <aryaning19@gmail.com>
2025-02-16 20:56:54 +0530

    hoja bhai

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-pyenv install 3.10.12
-pyenv global 3.10.12
+echo "Installing Python 3.10.12..."
+sudo apt-get update -y
+sudo apt-get install -y python3.10 python3.10-venv python3.10-dev
+
+echo "Setting Python 3.10 as default..."
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+sudo update-alternatives --config python3
+
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
